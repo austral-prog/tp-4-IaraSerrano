7d0c6dbdfbf367a7424eeeb6650a773e098d4c3a
IaraSerrano <163553535+IaraSerrano@users.noreply.github.com>
2024-04-19 10:08:51 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,8 @@
 def leap_year():
-    print("TO DO")
+    anio = int(input("igrese un anio:")
+    if anio % 4 ==0 or (anio % 400 == 0 or (anio % 400 != 0 and anio % 100 != 0)) :
+        print(f"El anio {anio} es bisiestio")
+    else:
+        print(f"El anio {anio} no es bisiestio")
+               
+    print(leap_year)
